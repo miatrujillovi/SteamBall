2e4e0160b6cb01385aaa8ac1f8b35a599fd8c1bc
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2025-03-11 08:35:27 -0600

    Basic Elements Added
    - Scripts defined
    - Created respective 2D elements
    - Added multiple colliders
    - Created Manager Scripts

--- Assets/Scripts/FlipperController.cs
+++ Assets/Scripts/FlipperController.cs
@@ -3,15 +3,13 @@ using UnityEngine.InputSystem;
 
 public class FlipperController : MonoBehaviour
 {
-    /* Script that allows the player to interact with the flippers using the property of Use Motor
-     * in Hinge Joint 2D. */
-
     //Bool to help differentiate between right and left flippers.
     public bool flipper;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Depending on the button pressive, it will activate the useMotor of the respective flipper
         /*if ((flipper && OnRight_Flipper()) || (!flipper && OnLeft_Flipper()))
         {
             GetComponent<HingeJoint2D>().useMotor = true;
